b273f003c21157eb3319ec079a144375edb0d863
pedromaranini <pedrolucasmaranini30@gmail.com>
2020-07-29 18:58:48 -0300

    Setup Inicial

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Menu from './componentes/menu';
+import dadosInicial from './data/dados_iniciais.json';
+import BannerMain from './componentes/BannerMain';
+import Corousel from './componentes/Carousel';
+import Footer from './componentes/Footer';
+
+function App() {
+  return (
+    <nav>
+      <Menu />
+      
+    </nav>
+  );
+}
+
+export default App;
